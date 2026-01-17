f662cac2686725c97a23e16ea0c90498c9c31189
Max Tovstopiatyi <maxrori228@gmail.com>
2026-01-17 17:18:28 +0200

    first simple code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./global.css";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function App() {
+  const [messageVisible, setMessageVisible] = useState(false);
+
+  const onPress = () => {
+    setMessageVisible(true);
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-xl font-bold text-blue-500">Hello Mate</Text>
+      <Pressable onPress={onPress} className="bg-white px-8 py-4 rounded-md">
+        <Text className="text-black text-lg font-bold">Press Me</Text>
+      </Pressable>
+
+      {messageVisible && (
+        <View className="absolute items-center bg-white px-24 py-20 rounded-md">
+          <Pressable
+            onPress={() => setMessageVisible(false)}
+            className="absolute right-2"
+          >
+            <Text className="text-black text-3xl font-bold">×</Text>
+          </Pressable>
+
+          <Text className="text-black text-2xl font-extrabold">Hello Mate</Text>
+        </View>
+      )}
     </View>
   );
 }
